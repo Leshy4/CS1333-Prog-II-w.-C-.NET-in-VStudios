a2764b266bcb808a47164af595a51c66c878dda7
Leshy4 <48464154+Leshy4@users.noreply.github.com>
2020-11-02 19:53:34 -0700

    Add files via upload

---
+++ Test.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApplication21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formAppearanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textAppearanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nerds Pizza";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Topping Chooser ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(269, 87);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Green Peppers";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(269, 112);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Chicken";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(269, 137);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(131, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Canadian Style Bacon";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(269, 162);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(85, 17);
+            this.checkBox4.TabIndex = 11;
+            this.checkBox4.Text = "Black Olives";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(269, 187);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(77, 17);
+            this.checkBox5.TabIndex = 12;
+            this.checkBox5.Text = "Red Onion";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Place My Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(85, 217);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Thin";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(85, 240);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Deep Dish";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(74, 102);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(57, 17);
+            this.rb1.TabIndex = 17;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Bit 14\"";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(74, 148);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(96, 17);
+            this.rb3.TabIndex = 19;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "Mega Byte 20\"";
+            this.rb3.UseVisualStyleBackColor = true;
+            this.rb3.CheckedChanged += new System.EventHandler(this.rb3_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(74, 125);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(66, 17);
+            this.rb2.TabIndex = 18;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Byte 16\"";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formAppearanceToolStripMenuItem,
+            this.textAppearanceToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(467, 24);
+            this.menuStrip1.TabIndex = 20;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formAppearanceToolStripMenuItem
+            // 
+            this.formAppearanceToolStripMenuItem.Name = "formAppearanceToolStripMenuItem";
+            this.formAppearanceToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.formAppearanceToolStripMenuItem.Text = "Form Appearance";
+            this.formAppearanceToolStripMenuItem.Click += new System.EventHandler(this.formAppearanceToolStripMenuItem_Click);
+            // 
+            // textAppearanceToolStripMenuItem
+            // 
+            this.textAppearanceToolStripMenuItem.Name = "textAppearanceToolStripMenuItem";
+            this.textAppearanceToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.textAppearanceToolStripMenuItem.Text = "Text Appearance";
+            this.textAppearanceToolStripMenuItem.Click += new System.EventHandler(this.textAppearanceToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Select Your Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Select A Crust";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 374);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Chris Welch Pizza Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formAppearanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textAppearanceToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Test.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        double size = 0;
+        double crust = 0;
+        double toppings = 0;
+        
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formAppearanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // ColorDialog.Equals = this.ForeColor;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            double pepper = 0.1;
+            pepper += toppings;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            double chicken = 0.2;
+            chicken += toppings;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            double bacon = 0.1;
+            bacon += toppings;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            double olive = .15;
+            olive += toppings;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            double onion = .05;
+            onion += toppings;
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            double bitSize = 7;
+            bitSize += size;
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            double byteSize = 8;
+            byteSize += size;
+        }
+
+        private void rb3_CheckedChanged(object sender, EventArgs e)
+        {
+            double mBsize = 10;
+            mBsize += size;        
+            
+        }
+
+       
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            double thin = 3;
+            thin += crust;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            double deepDish = 6;
+            deepDish += crust;
+
+        }
+
+
+        
+
+        private void textAppearanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // ColorDialog = this.Text;
+        }
+
+        public double myPizza()
+        {
+            double area = (size * 3.14);
+            double total = (crust + toppings);
+            return area + total;
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e, double area, double total)
+        {
+          MessageBox.Show("Your pizza is " + area + " square inches" + "\nYour pizza crust cost is $" + crust + "\nYour toppings cost is $" + toppings + "\nYour Total is $" + total);
+
+        }
+
+    }
+}
